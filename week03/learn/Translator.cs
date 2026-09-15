356a3fd25aedc9fe00bdd4c81bf5f091d357692f
mrlee1234567 <jarslee20021013@gmail.com>
2026-09-14 17:33:32 -0700

    did the learning activities

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -25,6 +25,7 @@ public class Translator
     public void AddWord(string fromWord, string toWord)
     {
         // ADD YOUR CODE HERE
+        _words.Add(fromWord,toWord);
     }
 
     /// <summary>
@@ -35,6 +36,15 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
+        string res;
+        if (_words.ContainsKey(fromWord))
+        {
+            res = _words[fromWord];
+        }
+        else
+        {
+            res = $"Das Wort \"{fromWord}\" ist nicht im Wörterbuch enhalten.";
+        }
+        return res;
     }
 }
